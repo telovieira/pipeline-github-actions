dc352bba05facccb4a78e5c0ba0b89e85249af13
telovieira <35201695+telovieira@users.noreply.github.com>
2023-07-18 16:30:58 -0300

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -14,8 +14,8 @@ resource "aws_instance" "vm" {
   ami                         = "ami-0a58342ee11775c33" # lpr-v7
   instance_type               = "t2.large"  
   key_name                    = data.aws_key_pair.key.key_name
-  subnet_id                   = data.terraform_remote_state.myvpc.outputs.subnet_id
-  vpc_security_group_ids      = [data.terraform_remote_state.myvpc.outputs.security_group_id]
+  subnet_id                   = "vpc-03a62f79ab03621de" # data.terraform_remote_state.myvpc.outputs.subnet_id
+  vpc_security_group_ids      = "sg-06d2b4ddff8bd7897" # [data.terraform_remote_state.myvpc.outputs.security_group_id]
   associate_public_ip_address = true
 
   tags = {
